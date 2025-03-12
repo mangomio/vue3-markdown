52464b4daabc4208055ce044b9285e2b63731f17
mangomio <2091904240@qq.com>
2025-03-13 00:00:12 +0800

    布局

--- vue3-markdown/eslint.config.js
+++ vue3-markdown/eslint.config.js
@@ -1,26 +1,26 @@
-import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import globals from 'globals'
+// import js from '@eslint/js'
+// import pluginVue from 'eslint-plugin-vue'
+// import globals from 'globals'
 
-export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
-  },
+// export default [
+//   {
+//     name: 'app/files-to-lint',
+//     files: ['**/*.{js,mjs,jsx,vue}'],
+//   },
 
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
+//   {
+//     name: 'app/files-to-ignore',
+//     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+//   },
 
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-    },
-  },
+//   {
+//     languageOptions: {
+//       globals: {
+//         ...globals.browser,
+//       },
+//     },
+//   },
 
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-]
+//   js.configs.recommended,
+//   ...pluginVue.configs['flat/essential'],
+// ]
